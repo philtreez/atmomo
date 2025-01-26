034f30d7b16f3a648f9d064d68cb68c82f19105c
philtreez <philipp.treezy@gmail.com>
2025-01-26 21:23:29 +0100

    n

---
+++ js/bubbles.js
@@ -0,0 +1,49 @@
+let bubbles = [];
+
+function setup() {
+  createCanvas(1000, 1000, WEBGL); // 3D-Canvas
+  for (let i = 0; i < 50; i++) { // Anzahl der Sphären
+    bubbles.push(new Bubble());
+  }
+  noStroke(); // Keine Umrandung der Sphären
+}
+
+function draw() {
+  background(200);
+
+  // Kamera-Steuerung mit der Maus
+  orbitControl();
+
+  // Zeichne alle Sphären
+  for (let bubble of bubbles) {
+    bubble.move();
+    bubble.show();
+  }
+}
+
+class Bubble {
+  constructor() {
+    this.x = random(-width / 2, width / 2); // Zufällige x-Position im 3D-Raum
+    this.y = height / 2 + random(100); // Startposition unten
+    this.z = random(-width / 2, width / 2); // Zufällige z-Position im 3D-Raum
+    this.size = random(10, 30); // Zufällige Größe der Sphäre
+    this.speed = random(1, 3); // Zufällige Geschwindigkeit
+  }
+
+  move() {
+    this.y -= this.speed; // Bewegung nach oben
+    if (this.y < -height / 2) { // Wenn die Sphäre den oberen Rand verlässt
+      this.y = height / 2 + this.size; // Zurück nach unten
+      this.x = random(-width / 2, width / 2); // Neue zufällige x-Position
+      this.z = random(-width / 2, width / 2); // Neue zufällige z-Position
+    }
+  }
+
+  show() {
+    push();
+    translate(this.x, this.y, this.z); // Position der Sphäre im 3D-Raum
+    fill(0, 255, 130, 150); // Halbtransparentes Grün
+    sphere(this.size); // Zeichne die Sphäre
+    pop();
+  }
+}
