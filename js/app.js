4c50750e0dfa7ded9c87a3a9746b893ee0cc364c
philtreez <philipp.treezy@gmail.com>
2025-01-26 20:28:45 +0100

    b

--- js/app.js
+++ js/app.js
@@ -76,7 +76,7 @@ function setupOscilloscope(context, device, outputNode) {
 
         for (let i = 0; i < bufferLength; i++) {
             const v = dataArray[i] / 256.0;
-            const y = (v * oscilloscopeCanvas.height * 0.5) + (oscilloscopeCanvas.height * 0.3); // Wellenform weiter oben (0.2 statt 0.5)
+            const y = (v * oscilloscopeCanvas.height * 0.3) + (oscilloscopeCanvas.height * 0.2); // Wellenform weiter oben (0.2 statt 0.5)
 
             // Grüne Fläche von der Wellenlinie nach unten
             oscilloscopeContext.lineTo(x, y);
